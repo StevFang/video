ad70917b5c2fad285ba8fbb5bce6a0ea9341a156
fbin <fangbin@pekon.com>
2019-01-04 22:30:52 +0800

    代码调整

--- src/main/java/com/qs/Application.java
+++ src/main/java/com/qs/Application.java
@@ -1,11 +1,8 @@
 package com.qs;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 启动类
@@ -13,10 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author FBin
  */
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableScheduling
 @EnableCaching
-@MapperScan(basePackages = "com.qs.dao")
 public class Application {
 
     public static void main(String[] args) {
