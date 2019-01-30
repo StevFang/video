0330cb60d6d42e085b049c2c34630772a843c4b0
fbin <fangbin@pekon.com>
2019-01-30 18:12:43 +0800

    代码调整

--- src/main/java/com/qs/service/command/ffmpeg/FfmpegOnlineCommandServiceImpl.java
+++ src/main/java/com/qs/service/command/ffmpeg/FfmpegOnlineCommandServiceImpl.java
@@ -17,18 +17,18 @@ import org.springframework.stereotype.Service;
 public class FfmpegOnlineCommandServiceImpl implements CommandService<LiveffmpegDTO> {
 
     @Override
-    public String createCommand(LiveffmpegDTO fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig) {
+    public String createCommand(LiveffmpegDTO liveffmpegDTO) {
         try{
-            String fastForwardMPEGPath = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getFfmpegPath();
+            String fastForwardMPEGPath = liveffmpegDTO.getFfmpegPath();
             if(StringUtils.isNotBlank(fastForwardMPEGPath)) {
                 // -i：输入流地址或者文件绝对地址
                 StringBuilder command = new StringBuilder(fastForwardMPEGPath).append(" -i ");
                 // 是否有必输项：输入地址，输出地址，应用名，twoPart：0-推一个元码流；1-推一个自定义推流；2-推两个流（一个是自定义，一个是元码）
-                String input = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getInput();
-                String output = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getOutput();
-                String appName = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getAppName();
-                String twoPart = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getTwoPart();
-                String codec = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getCodec();
+                String input = liveffmpegDTO.getInput();
+                String output = liveffmpegDTO.getOutput();
+                String appName = liveffmpegDTO.getAppName();
+                String twoPart = liveffmpegDTO.getTwoPart();
+                String codec = liveffmpegDTO.getCodec();
 
                 // 默认h264解码
                 codec = codec == null ? "h264" : codec;
@@ -41,18 +41,18 @@ public class FfmpegOnlineCommandServiceImpl implements CommandService<Liveffmpeg
                     command.append(" -vcodec ").append(codec).append(" -f flv -an ").append(output).append(appName);
                 } else {
                     // -f ：转换格式，默认flv
-                    String fmt = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getFmt();
+                    String fmt = liveffmpegDTO.getFmt();
                     if(StringUtils.isNotBlank(fmt)){
                         command.append(" -f ").append(fmt);
                     }
                     // -r :帧率，默认25；-g :帧间隔
-                    String fps = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getFps();
+                    String fps = liveffmpegDTO.getFps();
                     if (StringUtils.isNotBlank(fps)) {
                         command.append(" -r ").append(fps);
                         command.append(" -g ").append(fps);
                     }
                     // -s 分辨率 默认是原分辨率
-                    String rs = fastForwardMovingPictureExpertsGroupLiveConfigDTOConfig.getRs();
+                    String rs = liveffmpegDTO.getRs();
                     if (StringUtils.isNotBlank(rs)) {
                         command.append(" -s ").append(rs);
                     }
